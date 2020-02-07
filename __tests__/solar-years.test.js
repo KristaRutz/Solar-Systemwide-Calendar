abe63f66c6ff00f1158309efb22cd99394716ffe
Krista Rutz <rutzkri000@gmail.com>
2020-02-07 09:08:09 -0800

    test for correct conversion of a earth year age to same length of time in x planet years

--- __tests__/solar-years.test.js
+++ __tests__/solar-years.test.js
@@ -8,4 +8,10 @@ describe("Planet Calendar Class", () => {
     expect(pluto.yearLength).toEqual(248);
     expect(pluto).toEqual({planetName: "Pluto", yearLength: 248});
   })
+  test("should accept a length of time in earth years and correctly return it in x planet years", () => {
+    const pluto = new PlanetCalendar("Pluto", 248);
+    const earth = new PlanetCalendar("Earth", 1);
+    expect(pluto.getAgeOnPlanet(248)).toEqual(1);
+    expect(earth.getAgeOnPlanet(25.3)).toEqual(25.3);
+  })
 }) 

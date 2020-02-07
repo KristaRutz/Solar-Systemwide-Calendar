4a2451bca892de9f365985b77ec21f012ab99cfd
Krista Rutz <rutzkri000@gmail.com>
2020-02-07 14:47:22 -0800

    add jest for life expectancy list

--- __tests__/solar-years.test.js
+++ __tests__/solar-years.test.js
@@ -17,25 +17,27 @@ describe("Planet Calendar Class", () => {
     expect(pluto.convertToPlanetYears(248)).toEqual(1);
   })
   test("should return the user's remaining life expectancy on a given planet", () => {
-    expect(pluto.getLifeExpectancyLeft(25)).toEqual("0.19 years left");
+    expect(pluto.getLifeExpectancyLeft(25)).toEqual("0.19 more years expected");
   })
   test("should return the user's remaining life expectancy on earth", () => {
     const earth = new PlanetCalendar("Earth", 1)
-    expect(earth.getLifeExpectancyLeft(25)).toEqual("47.60 years left");
+    expect(earth.getLifeExpectancyLeft(25)).toEqual("47.60 more years expected");
   })
 }) 
 
 describe("Solar System Calendar Class", () => {
+  const calendar = new SolarSystemCalendar();
+
   test("should construct an object that contains an array of planet calendar objects for all planets in our solar system", () => {
-    const calendar = new SolarSystemCalendar();
     expect(calendar.planetCalendars.length).toEqual(8);
     expect(calendar.planetCalendars[0]).toEqual({planetName: "Mercury", yearLength: .241});
     expect(calendar.planetCalendars[0].planetName).toEqual("Mercury");
     expect(calendar.planetCalendars[5].convertToPlanetYears(25.3)).toEqual(25.3/29.456);
   })
   test("should take an age (in earth years) and return a string listing this timeframe on each planet in the solar system", () => {
-    const calendar = new SolarSystemCalendar();
     expect(calendar.getAgeList(50)).toEqual("Mercury: 207.47, Venus: 81.27, Earth: 50.00, Mars: 26.58, Jupiter: 4.22, Saturn: 1.70, Uranus: 0.60, Neptune: 0.30")
   })
-  //test("should tell a user their life expectancy")
+  test("should tell a user their life expectancy on each planet in the solar system", () => {
+    expect(calendar.getLifeExpectancyList(10)).toEqual("");
+  })
 })

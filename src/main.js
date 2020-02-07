5f7f348af881ee4eec6a65e3f3f04178811ea845
Krista Rutz <rutzkri000@gmail.com>
2020-02-07 14:49:36 -0800

    implement responsive UI

--- src/main.js
+++ src/main.js
@@ -5,13 +5,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 
 $(document).ready(function() {
-  $('#user-age').submit(function(event) {
-    event.preventDefault();
+  const cal = new SolarSystemCalendar();
 
+  $('#user-age').click(function() {
     let earthAge = $("#age").val();
-    const cal = new SolarSystemCalendar();
-    let mercuryAge = cal.mercuryCal.convertToPlanetYears(earthAge).toFixed(2);
-    $("#output").text(mercuryAge);
+    $("#age-output").text(cal.getAgeList(earthAge));
+    cal.venusCal.getLifeExpectancyLeft(25);
+  });
+
+  $('#user-life-expectancy').click(function() {
+    let earthAge = $("#age2").val();
+    $("#age-output").text(cal.getAgeList(earthAge));
+    cal.venusCal.getLifeExpectancyLeft(25);
   });
 });
 
